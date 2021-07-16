f0a8f6be16f14ade69904ee768d377b1ebcd48d1
Samiun Nafis <luea099@gmail.com>
2021-07-16 07:38:32 +0600

    Initial commit

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+var typedLogo = new Typed('#type', {
+	strings: ['Hey there, <span class="wave"></span>', '<span class="iam" style="display:inline;">I am</span> <span class="span" style="display:inline;">Samiun Nafis</span>'],
+	typeSpeed: 120,
+	backSpeed: 40,
+	backDelay: 600,
+	startDelay: 800,
+	cursorChar: '_',
+	loop: false
+});
+
+let options = {
+	distance: '150%',
+	origin: 'bottom',
+	opacity: null
+}
+
+ScrollReveal({ reset: false});
+ScrollReveal().reveal('.item', options);
+
 let changeTheme = () => {
 	if (theme.checked) {
 		document.body.style.background = '#121212'
